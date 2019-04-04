f06788498fe321b8767121cdc13763abde900dd8
Rami Hikmat <rami.salem@protonmail.com>
2019-04-04 05:29:37 +0000

    First Task

--- cipher_code.c
+++ cipher_code.c
@@ -3,6 +3,7 @@
 
 //This is where the function prototypes of the task are going to go. 
 
+char ERC(int A); // ERC - Encrypting Rotation Cipher, This function will try to do task 1. 
 
 
 
@@ -14,6 +15,7 @@ int main () {
     
     //These printf functions create the menu so the program has a user friendly interface. 
     //The numbers used represent the different options the user can input to get to different options. 
+    
     printf("Please select from the following numbers the option you want to choose\n");
     printf("1 - Encryption with a Rotation Cipher\n2 - Decryption with a Rotation Cipher\n3 - Encryption with a Substitution Cipher\n");
     printf("4 - Decryption with a Substitiution Cipher\n5 - Decryption of Encrypted Substitutional Cipher given only Cipher Text and Substitutions\n");
@@ -25,7 +27,9 @@ int main () {
     
     switch (c) {
         case 1: 
-        printf("1"); //The Printf placements is where the functions are going to go for each task. 
+        
+        printf("%d", ERC(A)); 
+        
         break; 
         case 2:
         printf("2"); 
@@ -45,9 +49,7 @@ int main () {
         default:
         printf("Please Type an Appropriate Input");
         break; 
-        
     }
-    
     
     
     
@@ -63,7 +65,22 @@ int main () {
 
 
 // Encryption of a message with a rotation cipher given the message text and rotation amount. 
+char ERC(int A) {
+    
 
+    
+    char k=1; //represents the shift in the alphabet or in order words the key. 
+    
+    int numbers[26] = {a=0, b=1, c=2, d=3, e=4, f=5, g=6, h=7, i=8, j=9, k=10, l=11, m=12, n=13, o=14, p=15, q=16, r=17, s=18, t=19, u=20, v=21, w=22, x=23, y=24, z=25};
+    
+    scanf("%d", &A);    
+    
+    A = (A+k)*%26
+    
+    return A; 
+    
+    
+}
 
 
 //Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount.
