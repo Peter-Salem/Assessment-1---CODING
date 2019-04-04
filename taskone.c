d8375bba3f8110843fb66c2acf1f4eb9a3c18e26
Rami Hikmat <rami.salem@protonmail.com>
2019-04-04 05:50:45 +0000

    Task 1

---
+++ taskone.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main () {
+    
+    int a, b, c, d, e; 
+    int Y;
+    int message[5] = {a=0, b=1, c=2, d =3, e =4};  
+    
+    Y = (X+K)*%26;
+    
+    
+    
+    
+}
