8b60f3d6215404d7c84884675c66f78acb95ff64
Rami Hikmat <rami.salem@protonmail.com>
2019-04-04 05:44:15 +0000

    Menu and Template

--- cipher_code.c
+++ cipher_code.c
@@ -3,10 +3,6 @@
 
 //This is where the function prototypes of the task are going to go. 
 
-char ERC(int A); // ERC - Encrypting Rotation Cipher, This function will try to do task 1. 
-
-
-
 
 
 int main () {
@@ -27,9 +23,7 @@ int main () {
     
     switch (c) {
         case 1: 
-        
-        printf("%d", ERC(A)); 
-        
+        printf("1"); 
         break; 
         case 2:
         printf("2"); 
@@ -65,22 +59,6 @@ int main () {
 
 
 // Encryption of a message with a rotation cipher given the message text and rotation amount. 
-char ERC(int A) {
-    
-
-    
-    char k=1; //represents the shift in the alphabet or in order words the key. 
-    
-    int numbers[26] = {a=0, b=1, c=2, d=3, e=4, f=5, g=6, h=7, i=8, j=9, k=10, l=11, m=12, n=13, o=14, p=15, q=16, r=17, s=18, t=19, u=20, v=21, w=22, x=23, y=24, z=25};
-    
-    scanf("%d", &A);    
-    
-    A = (A+k)*%26
-    
-    return A; 
-    
-    
-}
 
 
 //Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount.
