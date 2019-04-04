10adeb7af41eb629c617e4de89db54b986fa5963
Rami Hikmat <rami.salem@protonmail.com>
2019-04-04 04:41:06 +0000

    Menu Added

--- cipher_code.c
+++ cipher_code.c
@@ -1,33 +1,80 @@
 #include<stdio.h> 
 
+
 int main () {
     
-    const int max =100; 
-    char numbers[max+1]; 
-    int index; 
-    int j=0; 
     
-    for(index=0; index<=max; index++) {
-        numbers[index]=1;
-    }
+    int c; //The address at which the user input from the menu will be stored so that we can use it in the switch case function. 
     
-    for (index=2; index<=max; index++) {
+    printf("Please select from the following numbers the option you want to choose\n");
+    printf("1 - Encryption with a Rotation Cipher\n2 - Decryption with a Rotation Cipher\n3 - Encryption with a Substitution Cipher\n");
+    printf("4 - Decryption with a Substitiution Cipher\n5 - Decryption of Encrypted Substitutional Cipher given only Cipher Text and Substitutions\n");
+    printf("6 - Decryption of a message encrypted with a substituition cipher given cipher text only\n");
+    
+    
+    scanf("%d", &c);
+    
+    
+    switch (c) {
+        case 1: 
+        printf("1"); //The Printf placements is where the functions are going to go for each task. 
+        break; 
+        case 2:
+        printf("2"); 
+        break;
+        case 3:
+        printf("3"); 
+        break; 
+        case 4:
+        printf("4"); 
+        break; 
+        case 5: 
+        printf("5"); 
+        break; 
+        case 6: 
+        printf("6"); 
+        break; 
+        default:
+        printf("Please Type an Appropriate Input");
+        break; 
         
-        if (numbers[index]==1) {
-            
-            for (j=2*index; j<=index; j=j+index) {
-                numbers[j]=0; 
-                
-            }
-        }
     }
     
-    for(index=2; index<=max; index++) {
-        
-        if(numbers[index]==1) {
-            
-            printf("%d is Prime\n", index);
-        }
-    }
+    
+    
     
 }
+
+
+
+
+
+
+//THE FOLLOWING ARE THE TASKS, EACH SHOULD PROBABLY HAVE A FUNCTION OF ITS OWN THAT CAN BE REFERRED TO IN THE MAIN INT. 
+
+
+
+// Encryption of a message with a rotation cipher given the message text and rotation amount. 
+
+
+
+//Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount.
+
+
+
+// Encryption of a message with a substitution cipher given a message text and alphabet substitution. 
+
+
+
+//Decryption of a message encrypted with a substitution cipher given cipher text and substitutions.
+
+
+
+//Decryption of a message encrypted with a rotation cipher given cipher text only. 
+
+
+
+//Decryption of a message encrypted with a substituition cipher given cipher text only. 
+
+
+
